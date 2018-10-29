dc8eff9c05fe4f14e89b1801a6f8884814d8b5a4
Nike Dattani <dattani.nike@gmail.com>
2018-10-28 22:49:56 -0400

    Added 3x3 figs and updated rest of the figs.

--- metapostfigs/makepdf.sh
+++ metapostfigs/makepdf.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 for f in fig*.eps; do ./a2ping.pl $f; done
+#for f in fig*.eps; do epspdf $f; done
 
 pdfcrop --margins '0 0 -1000 -1000'       fig_Chimera1LL.pdf          fig_Chimera1LL_crop.pdf
 pdfcrop --margins '-500 -500 -500 -500'   fig_Pegasus1LLone.pdf       fig_Pegasus1LLone_crop.pdf
@@ -11,3 +12,14 @@ pdfcrop --margins '0 0 -1000 -1000'       fig_Chimera2LL.pdf          fig_Chimer
 pdfcrop --margins '-500 -500 -500 -500'   fig_Pegasus2LLone.pdf       fig_Pegasus2LLone_crop.pdf
 pdfcrop --margins '0 0 -1000 -1000'       fig_Pegasus2LLfull.pdf      fig_Pegasus2LLfull_crop.pdf
 pdfcrop --margins '0 0 -1000 -1000'       fig_Pegasus2LLfullblue.pdf  fig_Pegasus2LLfullblue_crop.pdf
+
+
+#pdfcrop --margins '0 0 -1000 -1000'       fig_Chimera1LL_3x3.pdf          fig_Chimera1LL_crop_3x3.pdf
+#pdfcrop --margins '-500 -500 -500 -500'   fig_Pegasus1LLone_3x3.pdf       fig_Pegasus1LLone_crop_3x3.pdf
+#pdfcrop --margins '0 0 -1000 -1000'       fig_Pegasus1LLfull_3x3.pdf      fig_Pegasus1LLfull_crop_3x3.pdf
+#pdfcrop --margins '0 0 -1000 -1000'       fig_Pegasus1LLfullblue_3x3.pdf  fig_Pegasus1LLfullblue_crop_3x3.pdf
+#
+#pdfcrop --margins '0 0 -1000 -1000'       fig_Chimera2LL_3x3.pdf          fig_Chimera2LL_crop-3x3.pdf
+#pdfcrop --margins '-500 -500 -500 -500'   fig_Pegasus2LLone_3x3.pdf       fig_Pegasus2LLone_crop_3x3.pdf
+#pdfcrop --margins '0 0 -1000 -1000'       fig_Pegasus2LLfull_3x3.pdf      fig_Pegasus2LLfull_crop_3x3.pdf
+#pdfcrop --margins '0 0 -1000 -1000'       fig_Pegasus2LLfullblue_3x3.pdf  fig_Pegasus2LLfullblue_crop_3x3.pdf
