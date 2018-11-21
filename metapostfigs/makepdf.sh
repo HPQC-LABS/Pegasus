1b07bf975969eab657e260b0bd21be0898ff3d39
Szilárd <szalay.szilard@wigner.mta.hu>
2018-11-21 10:30:06 +0100

    Pegasus graphs: added missing edges, .mp, .lyx, .pdf adjusted, finetunings

--- metapostfigs/makepdf.sh
+++ metapostfigs/makepdf.sh
@@ -7,18 +7,18 @@ pdfcrop --margins '-420 -300 -420 -300'	fig_PegasusDmdRect5x5one.pdf		fig_Pegasu
 pdfcrop --margins '-420 -300 -880 -680'	fig_PegasusDmdRect5x5full.pdf		fig_PegasusDmdRect5x5full_crop.pdf
 pdfcrop --margins '-420 -300 -880 -680'	fig_PegasusDmdRect5x5fullblue.pdf	fig_PegasusDmdRect5x5fullblue_crop.pdf
 
-pdfcrop --margins '-320 -320 -660 -660'	fig_ChimeraStdRect5x5.pdf		fig_ChimeraStdRect5x5_crop.pdf
-pdfcrop --margins '-320 -320 -320 -320'	fig_PegasusStdRect5x5one.pdf		fig_PegasusStdRect5x5one_crop.pdf
-pdfcrop --margins '-320 -320 -660 -660'	fig_PegasusStdRect5x5full.pdf		fig_PegasusStdRect5x5full_crop.pdf
-pdfcrop --margins '-320 -320 -660 -660'	fig_PegasusStdRect5x5fullblue.pdf	fig_PegasusStdRect5x5fullblue_crop.pdf
+pdfcrop --margins '-320 -380 -680 -800'	fig_ChimeraStdRect5x5.pdf		fig_ChimeraStdRect5x5_crop.pdf
+pdfcrop --margins '-320 -380 -320 -380'	fig_PegasusStdRect5x5one.pdf		fig_PegasusStdRect5x5one_crop.pdf
+pdfcrop --margins '-320 -380 -680 -800'	fig_PegasusStdRect5x5full.pdf		fig_PegasusStdRect5x5full_crop.pdf
+pdfcrop --margins '-320 -380 -680 -800'	fig_PegasusStdRect5x5fullblue.pdf	fig_PegasusStdRect5x5fullblue_crop.pdf
 
 pdfcrop --margins '-420 -380 -880 -780'	fig_ChimeraDmdTilt5x5.pdf		fig_ChimeraDmdTilt5x5_crop.pdf
 pdfcrop --margins '-420 -380 -420 -380'	fig_PegasusDmdTilt5x5one.pdf		fig_PegasusDmdTilt5x5one_crop.pdf
 pdfcrop --margins '-420 -380 -880 -780'	fig_PegasusDmdTilt5x5full.pdf		fig_PegasusDmdTilt5x5full_crop.pdf
 pdfcrop --margins '-420 -380 -880 -780'	fig_PegasusDmdTilt5x5fullblue.pdf	fig_PegasusDmdTilt5x5fullblue_crop.pdf
 
-pdfcrop --margins '-320 -400 -660 -760'	fig_ChimeraStdTilt5x5.pdf		fig_ChimeraStdTilt5x5_crop.pdf
-pdfcrop --margins '-320 -400 -320 -400'	fig_PegasusStdTilt5x5one.pdf		fig_PegasusStdTilt5x5one_crop.pdf
-pdfcrop --margins '-320 -400 -660 -760'	fig_PegasusStdTilt5x5full.pdf		fig_PegasusStdTilt5x5full_crop.pdf
-pdfcrop --margins '-320 -400 -660 -760'	fig_PegasusStdTilt5x5fullblue.pdf	fig_PegasusStdTilt5x5fullblue_crop.pdf
+pdfcrop --margins '-320 -460 -680 -960'	fig_ChimeraStdTilt5x5.pdf		fig_ChimeraStdTilt5x5_crop.pdf
+pdfcrop --margins '-320 -500 -320 -500'	fig_PegasusStdTilt5x5one.pdf		fig_PegasusStdTilt5x5one_crop.pdf
+pdfcrop --margins '-320 -460 -680 -960'	fig_PegasusStdTilt5x5full.pdf		fig_PegasusStdTilt5x5full_crop.pdf
+pdfcrop --margins '-320 -460 -680 -960'	fig_PegasusStdTilt5x5fullblue.pdf	fig_PegasusStdTilt5x5fullblue_crop.pdf
 
